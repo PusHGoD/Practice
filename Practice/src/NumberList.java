820aa7f44706c7f50be17f1796b13305fa3e7091
PusHGoD <vip_pro@live.com>
2016-10-21 16:21:45 +0700

    Add feature find Average

--- Practice/src/NumberList.java
+++ Practice/src/NumberList.java
@@ -1,3 +1,13 @@
 public class NumberList {
+	public static int findAvg(String a) {
+		StringTokenizer stk = new StringTokenizer(a, " ");
+		ArrayList arr = new ArrayList();
+		while (stk.hasMoreTokens()) {
+			arr.add(Integer.parseInt(stk.nextToken()));
+		}
+		Collections.sort(arr);
+		int avg = arr.size() / 2;
+		return (int) arr.get(avg);
+	}
 
 }
