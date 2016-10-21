42da92a10467af293c09cd277fc6ea5e1203cc63
PusHGoD <vip_pro@live.com>
2016-10-21 15:43:28 +0700

    Add feature 2

--- Practice/src/NumberList.java
+++ Practice/src/NumberList.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
 
 public class NumberList {
 
+	public static int getMin(int[] nums) {
+		if (nums.length > 0) {
+			int min = nums[0];
+			int size = nums.length;
+			for (int i = 1; i < size; i++) {
+				if (nums[i] < min) {
+					min = nums[i];
+				}
+			}
+			return min;
+		}
+		return -1;
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input list size: ");
+		int size = Integer.parseInt(sc.nextLine());
+		int[] nums = new int[size];
+		for (int i = 0; i < size; i++) {
+			System.out.print("Nums[" + (i + 1) + "]:");
+			String input = sc.nextLine();
+			if (input.matches("\\d+")) {
+				nums[i] = Integer.parseInt(input);
+			}
+		}
+		System.out.println("Max:" + findMax(nums));
+		System.out.println("Min:" + getMin(nums));
+		System.out.println("Avg:" + getMin(nums));
+		sc.close();
+	}
 }
