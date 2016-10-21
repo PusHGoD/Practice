b4c4f753100ba8738ed3db4fdf823d8bf71f12eb
DanhLT <onljne48h@gmail.com>
2016-10-21 15:40:30 +0700

    findAvg Danh

--- Practice/src/NumberList.java
+++ Practice/src/NumberList.java
@@ -1,4 +1,16 @@
-
 public class NumberList {
-
+	public int findAvg(String a){
+	StringTokenizer stk = new StringTokenizer(a, " ");
+		ArrayList arr = new ArrayList();
+		while (stk.hasMoreTokens()) {
+			arr.add(Integer.parseInt(stk.nextToken()));
+		}
+		Collections.sort(arr);
+		int avg = arr.size()/2;
+		for (Object obj : arr) {
+			System.out.print(obj.toString()+"--");
+		}
+		
+		return (int)arr.get(avg);
+}
 }
